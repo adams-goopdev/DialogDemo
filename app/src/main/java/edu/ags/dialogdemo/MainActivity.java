9153b1ed1b971f66fdedb7d13cada7630db23770
Adam-Schapekahm <60486516+Adam-Schapekahm@users.noreply.github.com>
2021-09-28 09:38:37 -0500

    Finished

--- app/src/main/java/edu/ags/dialogdemo/MainActivity.java
+++ app/src/main/java/edu/ags/dialogdemo/MainActivity.java
@@ -1,11 +1,15 @@
 package edu.ags.dialogdemo;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -17,8 +21,13 @@ import edu.ags.dialogdemo.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "myDebug";
+
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -32,9 +41,9 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+/*        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,16 +70,58 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            //Invoke the Dialog
+            addItemDialog();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
+    private void addItemDialog() {
+        Log.d(TAG, "addItemDialog: Start");
+
+        //Create a layoutinflator
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+
+        //Use the infaltor to create a view from the xml
+        final View addView = layoutInflater.inflate(R.layout.add,null);
+
+        //Build and show the view (dialog)
+        //Set properties
+        //Designate the OK and Cancel
+
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.additem)
+                .setView(addView)
+                .setPositiveButton(R.string.OK,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Get the input
+                                EditText txtAddItem = addView.findViewById(R.id.txtAddItem);
+
+                                // Put value into string
+                                String item = txtAddItem.getText().toString();
+
+                                //Add the item to the Textview
+                                TextView lblMain = findViewById(R.id.lblMain);
+                                lblMain.append("\n\n" + item);
+                            }
+                        })
+                .setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d(TAG, "onClick: setNegativeButton");
+                    }
+                }).show();
+
+    }
+
+/*    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    }*/
 }
